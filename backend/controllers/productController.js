4721742d21ad368866999072ff7c4b00a032c463
rozzdev <lightsamuel10@gmail.com>
2025-03-17 11:28:08 +0100

    update for admin page

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -14,10 +14,10 @@ const addProduct = async (req, res) => {
       bestSeller,
     } = req.body;
 
-    const image1 = req.files.image1 & req.files.image1[0];
-    const image2 = req.files.image2 & req.files.image2[0];
-    const image3 = req.files.image3 & req.files.image3[0];
-    const image4 = req.files.image4 & req.files.image4[0];
+    const image1 = req.files.image1 && req.files.image1[0];
+    const image2 = req.files.image2 && req.files.image2[0];
+    const image3 = req.files.image3 && req.files.image3[0];
+    const image4 = req.files.image3 && req.files.image4[0];
 
     const images = [image1, image2, image3, image4].filter(
       (item) => item !== undefined
@@ -47,9 +47,9 @@ const addProduct = async (req, res) => {
     console.log(productData);
 
     const product = new productModel(productData);
-    await product.save() 
+    await product.save();
 
-    res.json({success:true, message:"product Added"});
+    res.json({ success: true, message: "product Added" });
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: error.message });
@@ -57,12 +57,37 @@ const addProduct = async (req, res) => {
 };
 
 //function for list product
-const listProduct = async (req, res) => {};
+const listProduct = async (req, res) => {
+  try {
+    const products = await productModel.find({});
+    res.json({ success: true, products });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 //funtcion for removing product
-const removeProduct = async (req, res) => {};
+const removeProduct = async (req, res) => {
+  try {
+    await productModel.findByIdAndDelete(req.body.id);
+    res.json({ success: true, message: "product removed" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 //function for single product info
-const singleProduct = async (req, res) => {};
+const singleProduct = async (req, res) => {
+  try {
+    const { productId } = req.body;
+    const product = await productModel.findById(productId);
+    res.json({ success: true, product });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 export { addProduct, listProduct, removeProduct, singleProduct };
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -6,11 +6,12 @@ import {
   singleProduct,
 } from "../controllers/productController.js";
 import upload from "../middleware/multer.js";
+import adminAuth from "../middleware/adminAuth.js";
 
 const productRouter = express.Router();
 
 productRouter.post(
-  "/add",
+  "/add",adminAuth,
   upload.fields([
     { name: "image1", maxCount: 1 },
     { name: "image2", maxCount: 1 },
@@ -19,8 +20,8 @@ productRouter.post(
   ]),
   addProduct
 );
-productRouter.post("/list", listProduct);
-productRouter.post("/remove", removeProduct);
+productRouter.get("/list", listProduct);
+productRouter.delete("/remove",adminAuth, removeProduct);
 productRouter.post("/single", singleProduct);
 
 export default productRouter;
